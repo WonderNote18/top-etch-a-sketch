275daa718efb337aff82992f19b40a27d49bad46
WonderNote18 <hector.d.almanza@gmail.com>
2020-01-16 09:27:14 -0600

    Created .hover and .sketch classes in style.css. Edited how table cells were made in script.js.

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 let containerDiv = document.querySelector("#sketchContainer");
 
-let createGrid = function() {
+function createGrid() {
     for (let i=0; i < 16; i++) {
-        let tempDivRow = document.createElement("div");
-        tempDivRow.id = `Row ${i}`;
-        tempDivRow.className = `sketchRow`;
-        containerDiv.append(tempDivRow);
+        //let tempDivRow = document.createElement("div");
+        //tempDivRow.id = `Row ${i}`;
+        //tempDivRow.className = `sketchRow`;
+        //containerDiv.appendChild(tempDivRow);
 
         for (let j=0; j < 16; j++) {
             let tempDivItem = document.createElement("div");
             tempDivItem.id =  `${i+1}-${j+1}`;
             tempDivItem.className = `sketchItem`;
-            tempDivRow.appendChild(tempDivItem);
+            containerDiv.appendChild(tempDivItem);
         }
     }
 }
 
+function sketchEnter(e) {
+
+}
+
+function sketchExit(e) {
+
+}
+
 createGrid();
+
+const sketchCells = document.querySelectorAll('.sketchItem');
+sketchCells.forEach(cell => cell.addEventListener("mouseenter", sketchEnter));
+sketchCells.forEach(cell => cell.addEventListener("mouseleave", sketchExit));
