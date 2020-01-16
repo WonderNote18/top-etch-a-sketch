44f9fb31367eb9b41f56e9b400ffef71d7cc8c3a
WonderNote18 <hector.d.almanza@gmail.com>
2020-01-16 09:34:57 -0600

    Removed commented code on how to create table cells in script.js

--- script.js
+++ script.js
@@ -2,10 +2,6 @@ let containerDiv = document.querySelector("#sketchContainer");
 
 function createGrid() {
     for (let i=0; i < 16; i++) {
-        //let tempDivRow = document.createElement("div");
-        //tempDivRow.id = `Row ${i}`;
-        //tempDivRow.className = `sketchRow`;
-        //containerDiv.appendChild(tempDivRow);
 
         for (let j=0; j < 16; j++) {
             let tempDivItem = document.createElement("div");
